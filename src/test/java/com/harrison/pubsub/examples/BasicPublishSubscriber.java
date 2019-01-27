dbfebbc0dafb1e6aecb798faee047bee70070d24
Harrison Spain <contact@harrison-spain.com>
2019-01-27 19:11:16 +0000

    example publishsubscriber

---
+++ src/test/java/com/harrison/pubsub/examples/BasicPublishSubscriber.java
@@ -0,0 +1,32 @@
+package com.harrison.pubsub.examples;
+
+import com.harrison.pubsub.PublishSubscribeService;
+import com.harrison.pubsub.PublishSubscriber;
+import com.harrison.pubsub.helpers.TestingCallback;
+
+/**
+ * An example of a class that implements both {@link com.harrison.pubsub.Publisher} and {@link
+ * com.harrison.pubsub.Subscriber} (via {@link PublishSubscriber}. This class subscribes to {@link
+ * String}s.
+ */
+public class BasicPublishSubscriber implements PublishSubscriber<String> {
+
+  private TestingCallback callback;
+
+  public BasicPublishSubscriber(TestingCallback callback) {
+    this.callback = callback;
+
+    // Subscribe to Strings
+    PublishSubscribeService.subscribe(this, String.class);
+  }
+
+  @Override
+  public boolean isPublishLoopbackAllowed() {
+    return true; // When publishing a String, this objects receive method will be called.
+  }
+
+  @Override
+  public void receive(String data) {
+    this.callback.callback(data);
+  }
+}
--- src/test/java/com/harrison/pubsub/helpers/TestingCallback.java
+++ src/test/java/com/harrison/pubsub/helpers/TestingCallback.java
@@ -1,5 +1,8 @@
 package com.harrison.pubsub.helpers;
 
+/**
+ * This interface is used when testing to for callbacks from example subscriber receive methods.
+ */
 public interface TestingCallback {
 
   void callback(Object data);
